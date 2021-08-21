565be5586dea835985634a03d1e512bd4fa8d411
Adithya Sudarsan <asdrsnn@gmail.com>
2021-08-21 17:33:52 -0400

    Connect gorm to postgres db

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  fmt.Sprintf("host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("PGADMIN_LISTEN_PORT")),
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/GetVersion", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("0.1\n"))
@@ -27,7 +39,7 @@ func main() {
 	}
 
 	fmt.Printf("Starting server at :8080")
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	if err != nil {
 		if err != http.ErrServerClosed {
 			panic(err)
