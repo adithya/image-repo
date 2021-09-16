5e433634fa1723193da3cdea6c8824453ab2970c
Adithya Sudarsan <asdrsnn@gmail.com>
2021-09-16 15:34:25 -0400

    Get DB host frame environment variable

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ const GCPProjectID = "shopify-challenge-image-repo"
 func main() {
 	var err error
 	DB, err = gorm.Open(postgres.New(postgres.Config{
-		DSN:                  fmt.Sprintf("host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("PGADMIN_LISTEN_PORT")),
+		DSN:                  fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("PGADMIN_LISTEN_PORT")),
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
 	}), &gorm.Config{})
 
