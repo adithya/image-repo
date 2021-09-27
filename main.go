9b4cb6bf7c21dec6119d1cd57a4be286aa6f33c7
Adithya Sudarsan <asdrsnn@gmail.com>
2021-09-26 22:18:56 -0400

    Load env vars from file when running outside container (e.g. VSCode)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"cloud.google.com/go/storage"
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -29,6 +30,11 @@ var IsDebug bool
 const GCPProjectID = "shopify-challenge-image-repo"
 
 func main() {
+	// If IS_CONTAINER is not set to true, server is running on host machine, not container, need load required environment varaiables from .env file
+	if os.Getenv("IS_CONTAINER") != "true" {
+		godotenv.Load(".env")
+	}
+
 	var err error
 	DB, err = gorm.Open(postgres.New(postgres.Config{
 		DSN:                  fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("PGADMIN_LISTEN_PORT")),
